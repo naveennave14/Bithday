56d9d3c1f2a61108e4a57bb24b249e3a01168ac6
naveennave14 <dvsbehehejejhdjd@gmail.com>
2026-03-23 08:04:59 +0530

    Update btn.js

--- btn.js
+++ btn.js
@@ -1,31 +1,23 @@
-// List of button IDs in the order you want them clicked
 const buttonIds = [
-    'turn_on',
-    'play',
-    'bannar_coming',
-    'balloons_flying',
-    'cake_fadein',
-    'light_candle',
-    'wish_message',
-    'story'
+    'turn_on', 'play', 'bannar_coming', 'balloons_flying',
+    'cake_fadein', 'light_candle', 'wish_message', 'story'
 ];
 
 let index = 0;
 
 function removeIconFromButton(button) {
-    const icon = button.querySelector('i');   // change selector if needed
+    // Remove any element that looks like an icon
+    const icon = button.querySelector('i, img, .fa, .fas, .far, .fab, .icon, [class*="icon"]');
     if (icon) icon.remove();
 }
 
 function clickNext() {
     if (index >= buttonIds.length) return;
-
     const btn = document.getElementById(buttonIds[index]);
     if (btn) {
         btn.click();
         removeIconFromButton(btn);
     }
-
     index++;
     setTimeout(clickNext, 1500);
 }
